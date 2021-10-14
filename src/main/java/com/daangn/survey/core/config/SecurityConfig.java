9725be7cd304122927bc650b63feb01c674918e4
allen <wlq7410@naver.com>
2021-10-14 21:17:36 +0900

    Fix: cors disable

--- src/main/java/com/daangn/survey/core/config/SecurityConfig.java
+++ src/main/java/com/daangn/survey/core/config/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .httpBasic().disable()
+                .cors().disable()
                 .csrf().disable()
                 .exceptionHandling()
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint)
