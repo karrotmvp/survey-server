1b8f59064e038814f6c75fc2c773709546a5381f
allen <wlq7410@naver.com>
2021-10-19 18:11:26 +0900

    Fix: Security Web config

--- src/main/java/com/daangn/survey/core/config/WebConfig.java
+++ src/main/java/com/daangn/survey/core/config/WebConfig.java
@@ -9,6 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*");
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .maxAge(3600);
     }
 }
