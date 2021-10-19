db7328ee438b5e03262f9fa81fa60c9908db1374
allen <wlq7410@naver.com>
2021-10-19 18:39:40 +0900

    Fix: Cors Preflight 수정

--- src/main/java/com/daangn/survey/core/config/SecurityConfig.java
+++ src/main/java/com/daangn/survey/core/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsUtils;
 
 @Configuration
 @EnableWebSecurity
@@ -38,6 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
                 .antMatchers(EXCLUDED_URLS).permitAll()
                 .anyRequest().authenticated()
                 .and()
