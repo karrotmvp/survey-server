26b14aa7384aad29b5548881ff0e37670b9a9abe
allen <wlq7410@naver.com>
2021-10-29 12:21:43 +0900

    Feat: url 명칭 변경

--- src/main/java/com/daangn/survey/common/util/shorturl/controller/UrlController.java
+++ src/main/java/com/daangn/survey/common/util/shorturl/controller/UrlController.java
@@ -8,21 +8,20 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/daangn/short-url")
 public class UrlController {
 
     private final UrlConvertService urlConverter;
 
     // 이거 안 사용할 거 같은데 아닌가?
-    @GetMapping("/convert")
+    @GetMapping("/daangn/short-url/convert")
     @ResponseBody
     public ShortUrlResult convertShortUrl(@RequestParam(defaultValue = "") String urlStr){
         return urlConverter.getShortenUrl(urlStr.trim());
     }
 
-    @GetMapping("/path/{shortUrl}")
-    public String redirectToOriginUrl(@PathVariable String shortUrl){
+    @GetMapping("/redirect")
+    public String redirectToOriginUrl(@RequestParam String url){
 
-        return "redirect:" + urlConverter.getShortenUrl(shortUrl.trim()).getShortUrl().getOriginUrl();
+        return "redirect:" + urlConverter.getShortenUrl(url.trim()).getShortUrl().getOriginUrl();
     }
 }
