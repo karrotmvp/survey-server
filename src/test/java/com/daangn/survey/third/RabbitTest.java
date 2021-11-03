e88bc587a518476194dc73ba323d694b4cd77f44
allen <wlq7410@naver.com>
2021-11-03 15:27:51 +0900

    Feat: RabbitMq 테스트 제거

--- src/test/java/com/daangn/survey/third/RabbitTest.java
+++ src/test/java/com/daangn/survey/third/RabbitTest.java
@@ -8,19 +8,19 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-public class RabbitTest {
-
-    @Autowired
-    private RabbitTemplate rabbitTemplate;
-
-    @Autowired
-    private DefaultListener defaultListener;
-
-    @Test
-    public void messageWithRabbit(){
-        rabbitTemplate.convertAndSend(RabbitConfig.topicExchangeName, "default.test", "hello RabbitMq");
-        String text = (String) rabbitTemplate.receiveAndConvert(RabbitConfig.queueName);
-        System.out.println(text);
-    }
-}
+//@SpringBootTest
+//public class RabbitTest {
+//
+//    @Autowired
+//    private RabbitTemplate rabbitTemplate;
+//
+//    @Autowired
+//    private DefaultListener defaultListener;
+//
+//    @Test
+//    public void messageWithRabbit(){
+//        rabbitTemplate.convertAndSend(RabbitConfig.topicExchangeName, "default.test", "hello RabbitMq");
+//        String text = (String) rabbitTemplate.receiveAndConvert(RabbitConfig.queueName);
+//        System.out.println(text);
+//    }
+//}
