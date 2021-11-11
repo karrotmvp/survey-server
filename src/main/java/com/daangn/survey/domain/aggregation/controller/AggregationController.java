4f728affb7cc25efd2a4d4ef8e932506fdff7b7f
allen <wlq7410@naver.com>
2021-11-11 20:33:18 +0900

    Feat: Swagger 업데이트

--- src/main/java/com/daangn/survey/domain/aggregation/controller/AggregationController.java
+++ src/main/java/com/daangn/survey/domain/aggregation/controller/AggregationController.java
@@ -5,10 +5,12 @@ import com.daangn.survey.common.dto.ResponseDto;
 import com.daangn.survey.common.message.ResponseMessage;
 import com.daangn.survey.core.annotation.CurrentUser;
 import com.daangn.survey.domain.aggregation.model.*;
+import com.daangn.survey.domain.aggregation.model.individual.AdminResponseDetailDto;
 import com.daangn.survey.domain.aggregation.model.individual.SurveyResponsesBrief;
 import com.daangn.survey.domain.aggregation.service.AggregationService;
 import com.daangn.survey.domain.member.model.entity.Member;
 import com.daangn.survey.domain.response.service.ResponseService;
+import com.daangn.survey.domain.survey.model.dto.SurveyDto;
 import com.daangn.survey.domain.survey.model.dto.SurveySummaryDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -17,6 +19,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,15 +32,16 @@ import java.util.List;
 
 import static com.daangn.survey.common.message.ResponseMessage.READ_SURVEY_LIST;
 
+@Tag(name = "집계 엔드포인트")
 @RequiredArgsConstructor
-@RequestMapping("/aggregation")
+@RequestMapping("/api/v1//aggregation")
 @RestController
 public class AggregationController {
 
-    private final AdminService adminService;
     private final ResponseService responseService;
     private final AggregationService aggregationService;
 
+    // TODO: 권한 체크
     @Operation(summary = "답변 통계 조회", description = "설문 결과를 조회합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "답변 결과 조회 성공",
@@ -52,6 +56,12 @@ public class AggregationController {
 
     }
 
+    @Operation(summary = "답변 요약 정보 조회", description = "설문 답변 요약 정보를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요약 정보 조회 성공",
+                    content = @Content(schema = @Schema(implementation = SurveyResponsesBrief.class))),
+            @ApiResponse(responseCode = "401", description = "요약 정보 조회 실패 (권한 에러)", content = @Content)
+    })
     @GetMapping("/{surveyId}/responses/brief")
     public ResponseEntity<ResponseDto<SurveyResponsesBrief>> getResponsesBrief(@PathVariable Long surveyId){
 
@@ -59,8 +69,14 @@ public class AggregationController {
                 .body(ResponseDto.of(HttpStatus.OK, ResponseMessage.READ_RESPONSES_BRIEF, aggregationService.getSurveyResponsesBrief(surveyId)));
     }
 
+    @Operation(summary = "개별 답변 정보 조회", description = "개별 답변 정보를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "개별 답변 정보 조회 성공",
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = AdminResponseDetailDto.class)))),
+            @ApiResponse(responseCode = "401", description = "개별 답변 정보 조회 실패 (권한 에러)", content = @Content)
+    })
     @GetMapping("/individual/{responseId}")
-    public ResponseEntity<ResponseDto<?>> getIndividualResponse(@PathVariable Long responseId){
+    public ResponseEntity<ResponseDto<List<AdminResponseDetailDto>>> getIndividualResponse(@PathVariable Long responseId){
        return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.of(HttpStatus.OK, ResponseMessage.EXAMPLE, aggregationService.getIndividualSurveyResponse(responseService.getSurveyResponse(responseId))));
     }
 }
