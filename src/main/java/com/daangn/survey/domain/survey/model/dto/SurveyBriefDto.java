aedf24d6018f22c8e9ee240e2e5cb10fd6eedc44
allen <wlq7410@naver.com>
2021-12-02 15:19:52 +0900

    Feat: Brief property에 Json Ignore 추가

--- src/main/java/com/daangn/survey/domain/survey/model/dto/SurveyBriefDto.java
+++ src/main/java/com/daangn/survey/domain/survey/model/dto/SurveyBriefDto.java
@@ -2,6 +2,7 @@ package com.daangn.survey.domain.survey.model.dto;
 
 import com.daangn.survey.domain.member.model.dto.BizProfileDto;
 import com.daangn.survey.domain.member.model.entity.BizProfile;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
@@ -40,6 +41,7 @@ public class SurveyBriefDto {
     @Schema(description = "설문 생성일", required = true)
     private LocalDateTime createdAt;
 
+    @JsonIgnore
     public boolean isCoverImageNull(){
         return this.bizProfile.getCoverImageUrls().size() == 0;
     }
