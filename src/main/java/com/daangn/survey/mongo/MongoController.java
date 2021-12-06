d51374010087d7e23d10db488282f91600d4b791
allen <wlq7410@naver.com>
2021-12-06 12:25:41 +0900

    Docs: Mongo API Swagger에 추가

--- src/main/java/com/daangn/survey/mongo/MongoController.java
+++ src/main/java/com/daangn/survey/mongo/MongoController.java
@@ -6,13 +6,25 @@ import com.daangn.survey.core.annotation.CurrentUser;
 import com.daangn.survey.domain.aggregation.model.individual.SurveyResponsesBrief;
 import com.daangn.survey.domain.member.model.entity.Member;
 import com.daangn.survey.domain.survey.survey.model.dto.SurveyBriefDto;
+import com.daangn.survey.mongo.aggregate.AggregationQuestionMongo;
+import com.daangn.survey.mongo.aggregate.individual.IndividualQuestionMongo;
 import com.daangn.survey.mongo.response.dto.ResponseMongoDto;
+import com.daangn.survey.mongo.response.dto.ResponseMongoRequestDto;
 import com.daangn.survey.mongo.survey.SurveyMongo;
 import com.daangn.survey.mongo.survey.SurveySummaryMongoDto;
+import com.daangn.survey.mongo.survey.dto.SurveyMongoDto;
+import com.daangn.survey.mongo.survey.dto.SurveyMongoRequestDto;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonDeserializer;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,6 +39,7 @@ import java.util.Map;
 import static com.daangn.survey.common.message.ResponseMessage.READ_SURVEY_BRIEF;
 import static com.daangn.survey.common.message.ResponseMessage.READ_SURVEY_LIST;
 
+@Tag(name = "몽고 엔드포인트")
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/mongo")
 @RestController
@@ -42,8 +55,14 @@ public class MongoController {
     }
 
     // Survey
+    @Operation(summary = "설문 생성", description = "설문과 질문들을 저장합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "설문 저장 성공", content = @Content)})
     @PostMapping("/survey")
-    public ResponseEntity<ResponseDto<?>> insertSurvey(@CurrentUser Member member, @RequestBody Map<String, Object> requestBody){
+    public ResponseEntity<ResponseDto<?>> insertSurvey(
+            @Parameter(description = "Member", hidden = true) @CurrentUser Member member,
+            @Parameter(description = "requestBody", schema = @Schema(implementation = SurveyMongoRequestDto.class)) @RequestBody Map<String, Object> requestBody
+    ){
         SurveyMongo surveyMongo = gson.fromJson(gson.toJson(requestBody), SurveyMongo.class);
 
         surveyMongo.setMemberId(member.getId());
@@ -54,19 +73,41 @@ public class MongoController {
                 .body(ResponseDto.of(HttpStatus.OK, ResponseMessage.EXAMPLE, mongoService.insertSurvey(surveyMongo)));
     }
 
+    @Operation(summary = "설문 리스트 조회", description = "설문 리스트를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "설문 리스트 조회 성공",
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = SurveySummaryMongoDto.class)))),
+            @ApiResponse(responseCode = "401", description = "설문 리스트 조회 실패 (권한 에러)", content = @Content)
+    })
     @GetMapping("/survey")
-    public ResponseEntity<ResponseDto<List<SurveySummaryMongoDto>>> getSurveys(@Parameter(description = "Member", hidden = true) @CurrentUser Member member){
+    public ResponseEntity<ResponseDto<List<SurveySummaryMongoDto>>> getSurveys(
+            @Parameter(description = "Member", hidden = true) @CurrentUser Member member
+    ){
 
         return ResponseEntity.status(HttpStatus.OK)
                 .body(ResponseDto.of(HttpStatus.OK, READ_SURVEY_LIST, mongoService.findSurveysByMemberId(member.getId())));
     }
 
+    @Operation(summary = "설문 상세 조회", description = "설문 상세를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "설문 상세 조회 성공",
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = SurveyMongoDto.class)))),
+            @ApiResponse(responseCode = "404", description = "설문 엔티티 조회 실패", content = @Content),
+            @ApiResponse(responseCode = "401", description = "설문 리스트 조회 실패 (권한 에러)", content = @Content(schema = @Schema(hidden = true)))
+    })
     @GetMapping("/survey/{surveyId}")
-    public ResponseEntity<ResponseDto<?>> getSurvey(@PathVariable Long surveyId){
+    public ResponseEntity<ResponseDto<SurveyMongoDto>> getSurvey(@PathVariable Long surveyId){
         return ResponseEntity.status(HttpStatus.OK)
                 .body(ResponseDto.of(HttpStatus.OK, ResponseMessage.EXAMPLE, mongoService.findSurvey(surveyId)));
     }
 
+    @Operation(summary = "설문 요약 조회", description = "설문 요약 정보를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "설문 요약 정보 조회 성공",
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = SurveyBriefDto.class)))),
+            @ApiResponse(responseCode = "404", description = "설문 엔티티 조회 실패", content = @Content),
+            @ApiResponse(responseCode = "401", description = "설문 리스트 조회 실패 (권한 에러)", content = @Content(schema = @Schema(hidden = true)))
+    })
     @GetMapping("/brief/{surveyId}")
     public ResponseEntity<ResponseDto<SurveyBriefDto>> getSurveyBrief(@PathVariable Long surveyId){
 
@@ -74,8 +115,14 @@ public class MongoController {
     }
 
     // Response
+    @Operation(summary = "답변 저장", description = "답변을 저장합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "답변 저장 성공", content = @Content)})
     @PostMapping("/response")
-    public ResponseEntity<ResponseDto<?>> insertResponse(@CurrentUser Member member, @RequestBody Map<String, Object> requestBody){
+    public ResponseEntity<ResponseDto<?>> insertResponse(
+            @Parameter(description = "Member", hidden = true) @CurrentUser Member member,
+            @Parameter(description = "requestBody", schema = @Schema(implementation = ResponseMongoRequestDto.class)) @RequestBody Map<String, Object> requestBody
+    ){
         ResponseMongoDto responseMongoDto = gson.fromJson(gson.toJson(requestBody), ResponseMongoDto.class);
 
         responseMongoDto.setMemberId(member.getId());
@@ -86,19 +133,37 @@ public class MongoController {
     }
 
     // Aggregation
+    @Operation(summary = "답변 통계 조회", description = "설문 결과를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "답변 결과 조회 성공",
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = AggregationQuestionMongo.class)))),
+            @ApiResponse(responseCode = "401", description = "답변 결과 조회 실패 (권한 에러)", content = @Content)
+    })
     @GetMapping("/aggregate/{surveyId}")
-    public ResponseEntity<ResponseDto<?>> getAggregation(@PathVariable Long surveyId){
+    public ResponseEntity<ResponseDto<List<AggregationQuestionMongo>>> getAggregation(@PathVariable Long surveyId){
 
         return ResponseEntity.status(HttpStatus.OK)
                 .body(ResponseDto.of(HttpStatus.OK, ResponseMessage.EXAMPLE, mongoService.getAggregation(surveyId)));
     }
 
+    @Operation(summary = "개별 답변 정보 조회", description = "개별 답변 정보를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "개별 답변 정보 조회 성공",
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = IndividualQuestionMongo.class)))),
+            @ApiResponse(responseCode = "401", description = "개별 답변 정보 조회 실패 (권한 에러)", content = @Content)
+    })
     @GetMapping("/surveys/{surveyId}/individual/{responseId}")
-    public ResponseEntity<ResponseDto<?>> getIndividualResponse(@PathVariable Long surveyId, @PathVariable Long responseId){
+    public ResponseEntity<ResponseDto<List<IndividualQuestionMongo>>> getIndividualResponse(@PathVariable Long surveyId, @PathVariable Long responseId){
         return ResponseEntity.status(HttpStatus.OK)
                 .body(ResponseDto.of(HttpStatus.OK, ResponseMessage.EXAMPLE, mongoService.getIndividualResponseMongo(surveyId, responseId)));
     }
 
+    @Operation(summary = "답변 요약 정보 조회", description = "설문 답변 요약 정보를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요약 정보 조회 성공",
+                    content = @Content(schema = @Schema(implementation = SurveyResponsesBrief.class))),
+            @ApiResponse(responseCode = "401", description = "요약 정보 조회 실패 (권한 에러)", content = @Content)
+    })
     @GetMapping("/{surveyId}/responses/brief")
     public ResponseEntity<ResponseDto<SurveyResponsesBrief>> getResponsesBrief(@PathVariable Long surveyId){
 
